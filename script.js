a7f897c900bf86838dca643745a441ec31280176
gabriela garciaguirre <mgarciaguirre4@gmail.com>
2025-02-26 12:31:43 -0600

    creacion de formulario de libros y script

---
+++ script.js
@@ -0,0 +1,30 @@
+function agregarLibro() {
+    const titulo = document.getElementById("titulo").value;
+    const autor = document.getElementById("autor").value;
+    const genero = document.getElementById("genero").value;
+    const isbn = document.getElementById("isbn").value;
+  
+    const libro = {
+      titulo: titulo,
+      autor: autor,
+      genero: genero,
+      isbn: isbn,
+    };
+  
+    mostrarLibro(libro);
+    limpiarFormulario();
+  }
+  
+  function mostrarLibro(libro) {
+    const listaLibros = document.getElementById("listaLibros");
+    const nuevoLibro = document.createElement("li");
+    nuevoLibro.textContent = `Título: ${libro.titulo}, Autor: ${libro.autor}, Género: ${libro.genero}, ISBN: ${libro.isbn}`;
+    listaLibros.appendChild(nuevoLibro);
+  }
+  
+  function limpiarFormulario() {
+    document.getElementById("titulo").value = "";
+    document.getElementById("autor").value = "";
+    document.getElementById("genero").value = "";
+    document.getElementById("isbn").value = "";
+  }
